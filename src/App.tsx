9b202cbf6553aa04bcf52fa0171b9a12c0f1ae12
Anik <tanvir.h.anik@gmail.com>
2025-05-14 14:29:24 +0600

    title ui added breadcrumb needs fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import NavBar from './navbar';
+import { BreadCrumbSeparator,HelpIcon,BookmarkIcon,FavoriteIcon } from './icons';
 import {
+  VKBreadCrumbs,
   Dropdown,
   DropdownTrigger,
   DropdownMenu,
@@ -9,12 +11,45 @@ import {
   VKGroup,
 } from "@vivakits/react-components";
 
+const breadcrumbOptions = [
+		{
+			title: "Operations",
+			href: "#",
+		},
+		{
+			title: "Employee List",
+			href: "#",
+		},
+	];
+
 function App() {
+  
   return (
     <div>
       <NavBar/>
-      <div className='bg-green-100'>
-        <div className='title bg-yellow-100-'>title</div>
+      <div className='title'>
+        <div className='flex justify-between h-[120px] px-5 pb-[60px] pt2.5 bg-gradient-to-r from-[#496CDF] to-[#7160EF]'>
+          <div>
+            <div className='bread_crumb_wrapper'>
+              <VKBreadCrumbs separator={<BreadCrumbSeparator/>} 
+              underline={false}
+              options={breadcrumbOptions} 
+              style={{
+                item: "inline-flex items-center",
+                link: "py-1 text-white text-xs font-medium leading-none leading-[18px]",
+              }}
+              />
+            </div>
+            <div>
+              <h3 className='mt-0 text-xl font-bold text-white'>Leo_14 Chandra Messi_14 (000123454)</h3>
+            </div>
+          </div>
+          <div className='flex py-[10px] gap-5'>
+            <div className='flex justify-center items-center rounded-md bg-gray-50 bg-opacity-10 h-9 w-9 cursor-pointer'><HelpIcon/></div>
+            <div className='flex justify-center items-center rounded-md bg-gray-50 bg-opacity-10 h-9 w-9 cursor-pointer'><BookmarkIcon/></div>
+            <div className='flex justify-center items-center rounded-md bg-gray-50 bg-opacity-10 h-9 w-9 cursor-pointer'><FavoriteIcon/></div>
+          </div>
+        </div>
         <div className='content bg-gray-100'>content</div>
       </div>
     </div>
