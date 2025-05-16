0006c8f3888a1ee5734f3775d2575e023fe21ca7
Anik <tanvir.h.anik@gmail.com>
2025-05-16 12:37:06 +0600

    edit modal ui updated not functional yet

--- src/App.tsx
+++ src/App.tsx
@@ -98,28 +98,121 @@ const breadcrumbOptions = [
     )
   }
 
-  function EditModal({children}: { children: React.ReactNode }){
+  function EditModal({children,errors,control,register}){
     return (
       <Modal>
         <ModalTrigger>
           {children}
         </ModalTrigger>
-        <ModalContent className="w-[600px]">
-          <ModalHeader className="px-4">Modal Header</ModalHeader>
+        <ModalContent className="w-[450px]">
+          <ModalHeader className="px-4">Edit</ModalHeader>
           <ModalBody className="p-4">
-            <p className="text-gray-500">
-              Modal content.....
-            </p>
+            <FormInputs errors={errors} control={control} register={register}/>
           </ModalBody>
           <ModalFooter>
-            <VKButton variant="outline" onClick={() => {}} className="px-3 py-1.5">Cancel</VKButton>
-            <VKButton className="px-3 py-1.5">
+            <VKButton variant="light" size="md" rounded="md" className='px-8' onClick={() => {}}>Cancel</VKButton>
+            <VKButton variant="solid" size="md" rounded="md" className='px-10'>
               Save
             </VKButton>
           </ModalFooter>
         </ModalContent>
       </Modal>
     )
+  }
+
+  function FormInputs({errors,control,register}){
+    return(
+      <div className='flex flex-col gap-1'>
+      <div className='input_wrapper grid grid-cols-2 gap-4'>
+        
+      <VKInput size="md" label="Name" labelClassName="text-xs font-semibold" isRequired={true} placeholder="Enter Name" type='text' id='name' rounded="md" hasError={errors.name !== undefined} errorMessage={errors?.name?.message} {...register("name")}/>
+      
+      <Controller
+        name="relationship"
+        control={control}
+        render={({ field }) => (
+          <VKSelect
+            label="Relationship"
+            labelClassName="text-xs font-semibold"
+            isRequired={true}
+            placeholder="Select Relationship"
+            rounded="md"
+            size='md'
+            className="bg-white"
+            isSearchable={true}
+            dropdownIndicator=<ExpandIcon/>
+            hasError={errors.relationship !== undefined} 
+            errorMessage={errors?.relationship?.message}
+            options={relationshipOptions}
+            value={field.value}
+            onChange={field.onChange}
+          />
+        )}
+      />
+      
+      <Controller
+        name="gender"
+        control={control}
+        render={({ field }) => (
+          <VKSelect
+            label="Gender"
+            labelClassName="text-xs font-semibold"
+            isRequired={true}
+            placeholder="Select Gender"
+            rounded="md"
+            size='md'
+            isSearchable={true}
+            className="bg-white"
+            dropdownIndicator=<ExpandIcon/>
+            hasError={errors.gender !== undefined} 
+            errorMessage={errors?.gender?.message}
+            options={genderOptions}
+            value={field.value}
+            onChange={field.onChange}
+          />
+        )}
+      />
+        
+      <VKInput size="md" label="NID/SSN" labelClassName="text-xs font-semibold" placeholder="Enter NID/SSN" type='text' id='nid-ssn'  rounded="md" hasError={errors.nid !== undefined} errorMessage={errors?.nid?.message} {...register("nid")}/>
+      
+      <Controller
+        name="dob"
+        control={control}
+        render={({ field }) => (
+          <div className="flex flex-col">
+            <label htmlFor="dob" className="text-xs font-semibold mb-1 pb-1">
+              Date of Birth
+            </label>
+            <VKDatepicker
+              className="border-gray-200"
+              id="dob"
+              placeholder='Select Date'
+              value={field.value}
+              format="DD-MM-YYYY"
+              icon=<CalendarIcon/>
+              iconPosition="end"
+              onChange={(event) => {
+                field.onChange(event.date);
+              }}
+            />
+            {errors?.dob && (
+              <span className="text-xs text-red-500 mt-1">
+                {errors.dob.message}
+              </span>
+            )}
+          </div>
+        )}
+      />
+      <VKInput size="md" label="Profession" labelClassName="text-xs font-semibold" placeholder="Enter Profession" type='text' id='profession'  rounded="md" hasError={errors.profession !== undefined} errorMessage={errors?.profession?.message} {...register("profession")}/>
+        
+      <VKInput size="md" label="Contact No" labelClassName="text-xs font-semibold" placeholder="Enter Contact" type='number' id='contact'  rounded="md" hasError={errors.contact !== undefined} errorMessage={errors?.contact?.message} {...register("contact")}/>
+      </div>
+      <div className='check_wrapper pt-5'>
+        <VKCheckbox className='border-blue-300' size='sm' rounded="sm" color={'primary'} {...register("isEmergencyContact")}><span className='text-dropDownTextGray text-xs font-medium'>Emergency Contact</span></VKCheckbox>
+      </div>
+    </div>
+    )
+    
   }
   
 
@@ -208,95 +301,7 @@ function App() {
           <section className='w-full pb-5 pl-7 flex flex-col gap-5'>
             <div>
               <div className='family-form-wrapper w-full flex flex-col gap-4 p-5 rounded-xl bg-gray-50'>
-                <div className='flex flex-col gap-1'>
-                  <div className='input_wrapper grid grid-cols-2 gap-4'>
-                    
-                  <VKInput size="md" label="Name" labelClassName="text-xs font-semibold" isRequired={true} placeholder="Enter Name" type='text' id='name' rounded="md" hasError={errors.name !== undefined} errorMessage={errors?.name?.message} {...register("name")}/>
-                  
-                  <Controller
-                    name="relationship"
-                    control={control}
-                    render={({ field }) => (
-                      <VKSelect
-                        label="Relationship"
-                        labelClassName="text-xs font-semibold"
-                        isRequired={true}
-                        placeholder="Select Relationship"
-                        rounded="md"
-                        size='md'
-                        className="bg-white"
-                        isSearchable={true}
-                        dropdownIndicator=<ExpandIcon/>
-                        hasError={errors.relationship !== undefined} 
-                        errorMessage={errors?.relationship?.message}
-                        options={relationshipOptions}
-                        value={field.value}
-                        onChange={field.onChange}
-                      />
-                    )}
-                  />
-                  
-                  <Controller
-                    name="gender"
-                    control={control}
-                    render={({ field }) => (
-                      <VKSelect
-                        label="Gender"
-                        labelClassName="text-xs font-semibold"
-                        isRequired={true}
-                        placeholder="Select Gender"
-                        rounded="md"
-                        size='md'
-                        isSearchable={true}
-                        className="bg-white"
-                        dropdownIndicator=<ExpandIcon/>
-                        hasError={errors.gender !== undefined} 
-                        errorMessage={errors?.gender?.message}
-                        options={genderOptions}
-                        value={field.value}
-                        onChange={field.onChange}
-                      />
-                    )}
-                  />
-                    
-                  <VKInput size="md" label="NID/SSN" labelClassName="text-xs font-semibold" placeholder="Enter NID/SSN" type='text' id='nid-ssn'  rounded="md" hasError={errors.nid !== undefined} errorMessage={errors?.nid?.message} {...register("nid")}/>
-                  
-                  <Controller
-                    name="dob"
-                    control={control}
-                    render={({ field }) => (
-                      <div className="flex flex-col">
-                        <label htmlFor="dob" className="text-xs font-semibold mb-1 pb-1">
-                          Date of Birth
-                        </label>
-                        <VKDatepicker
-                          className="border-gray-200"
-                          id="dob"
-                          placeholder='Select Date'
-                          value={field.value}
-                          format="DD-MM-YYYY"
-                          icon=<CalendarIcon/>
-                          iconPosition="end"
-                          onChange={(event) => {
-                            field.onChange(event.date);
-                          }}
-                        />
-                        {errors?.dob && (
-                          <span className="text-xs text-red-500 mt-1">
-                            {errors.dob.message}
-                          </span>
-                        )}
-                      </div>
-                    )}
-                  />
-                  <VKInput size="md" label="Profession" labelClassName="text-xs font-semibold" placeholder="Enter Profession" type='text' id='profession'  rounded="md" hasError={errors.profession !== undefined} errorMessage={errors?.profession?.message} {...register("profession")}/>
-                    
-                  <VKInput size="md" label="Contact No" labelClassName="text-xs font-semibold" placeholder="Enter Contact" type='number' id='contact'  rounded="md" hasError={errors.contact !== undefined} errorMessage={errors?.contact?.message} {...register("contact")}/>
-                  </div>
-                  <div className='check_wrapper pt-5'>
-                    <VKCheckbox className='border-blue-300' size='sm' rounded="sm" color={'primary'} {...register("isEmergencyContact")}><span className='text-dropDownTextGray text-xs font-medium'>Emergency Contact</span></VKCheckbox>
-                  </div>
-                </div>
+                <FormInputs errors={errors} control={control} register={register}/>
                 <div className='submit_button_wrapper'>
                   <VKButton onClick={handleSubmit(onSubmit)} size="md" rounded="md" className='px-10'>
                     Save info
@@ -325,7 +330,7 @@ function App() {
                       <td className="py-2.5 px-5 w-1/5 text-sm font-medium text-gray-500 break-all">{family.isEmergencyContact ? "Yes" : "No"}</td>
                       <td className="py-2.5 px-5 w-1/5">
                       <div className="flex items-center w-full justify-end">
-                        <EditModal>
+                        <EditModal errors={errors} control={control} register={register}>
                           <div 
                           className='edit_icon_wrapper flex items-center justify-center h-8 w-8 cursor-pointer rounded-md hover:bg-blue-50'
                           onClick={()=>{}}
